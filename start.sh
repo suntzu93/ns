9666f4a27baf4fee3d331652ba9243a0d077873e
Suntzu <thanhlv93@gmail.com>
2025-02-20 16:03:20 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# Function to run the program and process its output
 run_program() {
+    # Define variables for toption and node_id
+    toption="2"
+    node_id="1048321"
+
     # Start the program and capture its output
     output=$(cargo run -r -- start --env beta 2>&1 < /dev/null)
     
     if echo "$output" | grep -q "y/n"; then
-        # If we see y/n prompt, send y,2,1280438
-        printf 'y\n2\n1048321\n' | cargo run -r -- start --env beta
+        # If we see y/n prompt, send y, toption, and node_id
+        printf 'y\n%s\n%s\n' "$toption" "$node_id" | cargo run -r -- start --env beta
     else
-        # First time or normal run, send 2,1280438
-        printf '2\n1048321\n' | cargo run -r -- start --env beta
+        # First time or normal run, send toption and node_id
+        printf '%s\n%s\n' "$toption" "$node_id" | cargo run -r -- start --env beta
     fi
 }
 
